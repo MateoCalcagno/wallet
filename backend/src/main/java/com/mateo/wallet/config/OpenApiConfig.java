43755406786c431d6a8a0217e09d5fc6616928c9
MateoCalcagno <mateocalcagno5@gmail.com>
2026-06-12 17:51:44 -0300

    feat: configure JWT in Swagger UI

--- backend/src/main/java/com/mateo/wallet/config/OpenApiConfig.java
+++ backend/src/main/java/com/mateo/wallet/config/OpenApiConfig.java
@@ -1,19 +1,35 @@
 package com.mateo.wallet.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI walletApi() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .name("bearerAuth")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearerAuth");
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Wallet API")
                         .description("Virtual wallet built with Spring Boot")
-                        .version("1.0.0"));
+                        .version("1.0.0"))
+                .addSecurityItem(securityRequirement)
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", securityScheme));
     }
 }
